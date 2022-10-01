70f12e5e8c2e3f1af272fb880fd579e8531f947a
Sadafsaiyed <114169478+Sadafsaiyed@users.noreply.github.com>
2022-10-01 23:27:45 +0530

    Question on Array
    printing the series using three variables.

---
+++ DAY 4.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int sum =a+b;
+            int temp=0;
+            for(int j=1;j<=n;j++){
+                System.out.print(sum+" ");
+                temp =(int)(Math.pow(2,j)*b);
+                sum = sum+temp;
+            }
+            System.out.println();
+            sum=0;
+        }
+        in.close();
+    }
+}
